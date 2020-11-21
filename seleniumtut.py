325eb3a11d1b9d663a90ba235e58b65a308e2ec4
sashank <sashanksiwakoti1@gmail.com>
2020-11-21 10:25:56 +0545

    with TWT tuts

--- seleniumtut.py
+++ seleniumtut.py
@@ -1,13 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 path='/Users/sashanksiwakoti/Desktop/python/selenium/chromedriver'
 driver=webdriver.Chrome(path)
 driver.get('https://hamrobazaar.com')
 search=driver.find_element_by_name('searchword')
 search.send_keys('duke 250')
 search.send_keys(Keys.RETURN)
-main=driver.find_element_by_xpath('//table')
-print(main.text)
+try:
+    main=WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,'//table')))
+    print(main.text)
+
+finally:
+    driver.quit()
+#main=driver.find_element_by_xpath('//table')
+#print(main.text)
 #time.sleep(5)
-driver.quit()
