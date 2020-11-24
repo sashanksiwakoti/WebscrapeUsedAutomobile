aa7e430c9c35cee52babe13c6f430eba2d3796b6
sashank <sashanksiwakoti1@gmail.com>
2020-11-24 17:44:35 +0545

    new page 1 list

--- seleniumtut.py
+++ seleniumtut.py
@@ -5,16 +5,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+finp=input('enter item you want to search: ')
 path='/Users/sashanksiwakoti/Desktop/python/selenium/chromedriver'
 driver=webdriver.Chrome(path)
 driver.get('https://hamrobazaar.com')
 search=driver.find_element_by_name('searchword')
-search.send_keys('duke 250')
+search.send_keys(finp)
 search.send_keys(Keys.RETURN)
 try:
     main=WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,'//table')))
-    print(main.text)
-
+    #header1st=main.find_element_by_xpath('//table/tbody/tr[2]/td/table[4]/tbody/tr/td[2]/table[5]')
+    for i in range(5, 21):
+        header=main.find_element_by_xpath('//table/tbody/tr[2]/td/table[4]/tbody/tr/td[2]/table[%s]/tbody/tr[1]/td[3]/a[1]'%i)
+        print(header.text)
 finally:
     driver.quit()
 #main=driver.find_element_by_xpath('//table')
